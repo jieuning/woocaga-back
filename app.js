a833b483753180f59783e5e56575b21236f1bdf8
hwang jieun <balllling92@gmail.com>
2024-02-11 20:17:19 +0900

    몽고 디비 셋팅 완료

---
+++ app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const port = process.env.PORT;
+const uri = process.env.MONGO_CONNECTION;
+
+const app = express();
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(cors());
+
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log("MongoDB 연결 성공");
+  })
+  .catch((err) => {
+    console.log("MongoDB 연결 실패 : ", err);
+  });
+
+app.listen(port, () => {
+  console.log(`server on port ${port}`);
+});
+
+module.exports = app;
